32b04406c08c07d1d15a8fa69663fbdbb42fe29c
Aish Srinivas <aishwarya-sri99@hotmail.com>
2021-12-29 16:46:01 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import gradio as gr
 
 
+# Callback function to use the model
 def image_to_text(image):
-
     description = 'A sample description'
     return description
 
 
-iface = gr.Interface(
+# Define UI interface
+iFace = gr.Interface(
+    title='Describia',
+    theme ='grass',
     fn=image_to_text,
-    inputs='image',
-    outputs='text'
+    inputs=gr.inputs.Image(label='Your image'),
+    outputs=gr.outputs.Textbox(label="What I think ..."),
+    allow_screenshot=False,
+    allow_flagging=False
 )
-iface.launch()
+
+# Launch interface
+iFace.launch()
