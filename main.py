e1fc6ae2c0cd3d359dd0a813c20c9cdb21a7cd75
Aish Srinivas <aishwarya-sri99@hotmail.com>
2021-12-30 20:19:27 -0500

    Make code modular

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-import gradio as gr
-import imageToDescriptionModel as imd
+# import gradio as gr
+import prepareImageData as prepI
+import prepareTextData as prepT
+import developModel as devM
+
+
+def setModel():
+    print("Preparing image data ...")
+    # prepI.prepareImageData()
+
+    print(' ')
+
+    print("Preparing text data ...")
+    prepT.prepareTextData()
+
+    print(' ')
+
+    print("Preparing model ...")
+    devM.prepareModel()
 
 
 # Callback function to use the model
@@ -8,18 +25,27 @@ def image_to_text(image):
     return description
 
 
-# Define UI interface
-iFace = gr.Interface(
-    title='Describia',
-    theme ='default',
-    fn=image_to_text,
-    inputs=gr.inputs.Image(label='Your image'),
-    outputs=gr.outputs.Textbox(label="What I think ..."),
-    allow_screenshot=False,
-    allow_flagging=False
-)
+def startUI():
+    # Define UI interface
+    iFace = gr.Interface(
+        title='Describia',
+        theme='default',
+        fn=image_to_text,
+        inputs=gr.inputs.Image(label='Your image'),
+        outputs=gr.outputs.Textbox(label="What I think ..."),
+        allow_screenshot=False,
+        allow_flagging=False
+    )
 
-# Launch interface
-iFace.launch(
-    share=True
-)
+    # Launch interface
+    iFace.launch(
+        share=True
+    )
+
+
+def main():
+    setModel()
+
+
+if __name__ == "__main__":
+    main()
